555e5ed53666ee65200ecc632b4174f60cbccc8f
dennis.menjivar <dennismenjivar@Denniss-MacBook-Pro.local>
2018-05-11 14:11:49 -0600

    Se agregaron los botones de opciones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
+// import { Component } from '@angular/core';
+// import { NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams, Toast, LoadingController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  options:string[] = ['Contactos','Recordatorios','Ejercicios','Noticias','Musica','Alertas','Ajuster','Comunidad','Beneficios','Juegos']
+  
   constructor(public navCtrl: NavController) {
-    
+
   }
 
 }
